f280e6820758d49b14dea809a9fc83bcabc5f26c
ItzJ3rmu <jere.santanen@gmail.com>
2015-01-20 09:55:04 +0200

    Kohta toimii
    Niin lähellä

--- twitter_juttu.py
+++ twitter_juttu.py
@@ -10,16 +10,16 @@ a2 = "miten"
 a3 = "Suomen"
 
 b1 = "lol"
-b2 = "miten"
-b3 = "Suomen"
+b2 = "how"
+b3 = "lels"
 
 c1 = "Vaalit"
 c2 = "miten"
 c3 = "moi"
 
-teema1 = 0
-teema2 = 0
-teema3 = 0
+teema1 = {}
+teema2 = {}
+teema3 = {}
 
 for tiedosto in sys.argv[1:]:
 
@@ -39,21 +39,31 @@ for tiedosto in sys.argv[1:]:
 
             teema_lista = tweet['text']
 
+            if aika not in teema1:
+                teema1[aika] = 0
+
+            if aika not in teema2:
+                teema2[aika] = 0
+
+            if aika not in teema3:
+                teema3[aika] = 0
+        
             if a1 in teema_lista or a2 in teema_lista or a3 in teema_lista:
-                teema1 = teema1 + 1
+                teema1[aika] += 1
 
             if b1 in teema_lista or b2 in teema_lista or b3 in teema_lista:
-                teema2 = teema2 + 1
-            
+                teema2[aika] += 1
+             
             if c1 in teema_lista or c2 in teema_lista or c3 in teema_lista:
-                teema3 = teema3 + 1
+                teema3[aika] += 1
 
         ##print teema_lista
-print teema1
-print teema2
-print teema3
-for paiva in sorted( laskuri.keys() ):
-    print paiva , "," , laskuri[ paiva ], "," , teema1, ",", teema2, ",", teema3
+    #print teema1
+    #print teema2
+    #print teema3
+
+    for paiva in sorted( laskuri.keys()) and theme1 in sorted(teema1.keys()):
+        print paiva , "," , laskuri[ paiva ], "," , teema1[theme1], ",", "teema2", ",", "teema3"
 
 
 
