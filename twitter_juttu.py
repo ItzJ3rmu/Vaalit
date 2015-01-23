fb3abda2530b7203394244016812ef8a0186a77a
ItzJ3rmu <jere.santanen@gmail.com>
2015-01-23 14:24:59 +0200

    Toimii hyvin
    Jeeee vihdoin :D

--- twitter_juttu.py
+++ twitter_juttu.py
@@ -15,6 +15,9 @@ isoteema = len(kaikki_teemat)
 
 laskuri_teemat = []
 
+tiedosto = "export.1884.json"
+
+
 for tiedosto in sys.argv[1:]:
 
     with open(tiedosto) as json_data:
@@ -27,7 +30,7 @@ for tiedosto in sys.argv[1:]:
                 ).strftime('%Y-%m-%d')
 
             if aika not in laskuri:
-                laskuri[aika] = 0
+               laskuri[aika] = 0
 
             laskuri[aika] += 1
 
@@ -39,12 +42,22 @@ for tiedosto in sys.argv[1:]:
 
                 if aika not in laskuri_teemat[y]:
                     laskuri_teemat[y][aika] = 0
+
             
                 flag = False
                 for x in kaikki_teemat[y]:
                     if x in teema_lista and flag == False:
                         laskuri_teemat[y][aika] += 1
                         flag = True
-
+                    
     for paiva in sorted( laskuri.keys()):
-        print paiva , "," , laskuri[ paiva ], "," , laskuri_teemat[0][paiva], "," , laskuri_teemat[1][paiva], "," , laskuri_teemat[2][paiva]
+        tarkastus = ''
+        for tulostus in range(0, isoteema):
+            tarkastus += str(laskuri_teemat[tulostus][paiva]) + " , "
+        print paiva , "," , laskuri[ paiva ], "," , tarkastus
+
+
+
+
+
+
