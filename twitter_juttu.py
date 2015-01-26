625b1e1882041b9d82ad8b667a1b02689f7ce0aa
ItzJ3rmu <jere.santanen@gmail.com>
2015-01-26 13:04:15 +0200

    Kokeilua

--- twitter_juttu.py
+++ twitter_juttu.py
@@ -5,23 +5,29 @@ import sys
 
 laskuri = {}
 
-a = ["Vaalit", "miten", "Suomen", "Miten"]
-b = ["tavoittelu", "kiva", "Sininen"]
-c = ["odottaa", "on", "moi"]
+teemat = "teemat.txt"
 
-kaikki_teemat = [a, b, c]
+f = open(teemat, 'r')
 
-isoteema = len(kaikki_teemat)
+jako = []
 
-laskuri_teemat = []
+for line in f:
+    line = line.strip()
+    jako.append(line.split(","))
+
+kaikki_teemat = [jako]
+
+teema_lasku = len(kaikki_teemat)
+
+teemat_lista = []
 
 for tiedosto in sys.argv[1:]:
 
     with open(tiedosto) as json_data:
-        d = json.load(json_data)
+        date = json.load(json_data)
         json_data.close()
 
-        for tweet in d:
+        for tweet in date:
             aika = datetime.datetime.fromtimestamp(
                     int(tweet['time'])
                 ).strftime('%Y-%m-%d')
@@ -31,27 +37,27 @@ for tiedosto in sys.argv[1:]:
 
             laskuri[aika] += 1
 
-            teema_lista = tweet['text']
+            teksti_lista = tweet['text']
         
-            for y in range(0, isoteema):
+            for y in range(0, teema_lasku):
 
-                laskuri_teemat.append({}) 
+                teemat_lista.append({}) 
 
-                if aika not in laskuri_teemat[y]:
-                    laskuri_teemat[y][aika] = 0
+                if aika not in teemat_lista[y]:
+                    teemat_lista[y][aika] = 0
 
             
                 flag = False
                 for x in kaikki_teemat[y]:
-                    if x in teema_lista and flag == False:
-                        laskuri_teemat[y][aika] += 1
+                    if x in teksti_lista and flag == False:
+                        teemat_lista[y][aika] += 1
                         flag = True
                     
     for paiva in sorted( laskuri.keys()):
-        tarkastus = ''
-        for tulostus in range(0, isoteema):
-            tarkastus += str(laskuri_teemat[tulostus][paiva]) + " , "
-        print paiva , "," , laskuri[ paiva ], "," , tarkastus
+        teema_tulostus = ''
+        for tulostus in range(0, teema_lasku):
+            teema_tulostus += str(teemat_lista[tulostus][paiva]) + " , "
+        print paiva , "," , laskuri[ paiva ], "," , teema_tulostus
 
 
 
